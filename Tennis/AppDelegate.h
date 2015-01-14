f87af9920621d32faec5140d160a232275127945
oliver <gushedaoren@gmail.com>
2015-01-14 21:10:01 +0800

    Initial Commit

---
+++ Tennis/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tennis
+//
+//  Created by oliver on 1/14/15.
+//  Copyright (c) 2015 oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tennis/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tennis
+//
+//  Created by oliver on 1/14/15.
+//  Copyright (c) 2015 oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
